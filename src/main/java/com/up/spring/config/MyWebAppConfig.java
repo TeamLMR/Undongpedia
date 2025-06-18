bfe9d991943491d8334d76fb66c99f8d3da7efb3
ddanggoos <hanck9595@gmail.com>
2025-06-18 11:15:02 +0900

    예외처리 기능추가

--- src/main/java/com/up/spring/config/MyWebAppConfig.java
+++ src/main/java/com/up/spring/config/MyWebAppConfig.java
@@ -1,9 +1,14 @@
 package com.up.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
+
+import java.util.Properties;
 
 @Configuration //설정클래스를 선언하는 어노테이션 == springbean-configuration.xml
 @EnableWebMvc //mvc:annotation-driven 설정과 동일
@@ -12,5 +17,20 @@ public class MyWebAppConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/enrollmember.do").setViewName("member/memberEnroll");
+    }
+
+    //    ExceptionHandler
+    @Bean
+    public HandlerExceptionResolver exceptionResolver() {
+        Properties prop = new Properties();
+
+//        prop.setProperty(IllegalArgumentException.class.getName(),"common/error/error1");
+//        prop.setProperty(NumberFormatException.class.getName(),"common/error/error2");
+
+        SimpleMappingExceptionResolver exceptionResolver = new SimpleMappingExceptionResolver();
+
+        exceptionResolver.setExceptionMappings(prop);
+
+        return exceptionResolver;
     }
 }
