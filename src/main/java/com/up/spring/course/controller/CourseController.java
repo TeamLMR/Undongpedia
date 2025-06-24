c3812c64fe42ad73bc71a6590445736cf080281d
ddanggoos <hanck9595@gmail.com>
2025-06-24 18:03:56 +0900

    코스 컨트롤러 이동, 코스 모델 추가

--- src/main/java/com/up/spring/course/controller/CourseController.java
+++ src/main/java/com/up/spring/course/controller/CourseController.java
@@ -1,4 +1,4 @@
-package com.up.spring.course;
+package com.up.spring.course.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
---
+++ src/main/java/com/up/spring/course/model/dto/Course.java
@@ -0,0 +1,14 @@
+package com.up.spring.course.model.dto;
+
+public class Course {
+    private int courseSeq; //제목
+    private String courseTitle; //제목
+    private String courseContent; //내용
+    private int courseCategory; //카테고리번호
+    private int courseDifficult; //난이도
+    private int coursePrice; // 가격
+    private int courseDiscount; // 할인율
+    private String courseThumbnail; // 썸네일
+    private String courseTarget; // 교육대상
+    private String coursePreparation; // 준비물
+}
