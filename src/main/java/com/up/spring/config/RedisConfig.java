60b65e3a6f62a3d0acb2efbe55ca59d0808af0a0
parkjisong <pjs96525@hanmail.net>
2025-07-06 18:30:40 +0900

    07/06 오류수정중..

--- src/main/java/com/up/spring/config/RedisConfig.java
+++ src/main/java/com/up/spring/config/RedisConfig.java
@@ -31,7 +31,7 @@ import java.time.Duration;
 
 @Configuration
 @EnableRedisRepositories
-@PropertySource("classpath:/config/server/redis.properties")
+@PropertySource("classpath:/config/local/redis.properties")
 public class RedisConfig {
 
     @Value("${redis.host}")
